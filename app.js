900c2f0d5637a3b7dcbd98fe0ff0f042356c496f
Ryan Yang <ryanyanyang@live.com>
2016-10-27 22:48:39 -0700

    Lucky is a monster

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get('/', function (req, res) {
 	res.sendFile(__dirname +'/static/editprofile.html');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
 	console.log('Example app listening on port 3000!');
 });

d21c150d1bf8bb2ed50f248bf0685ff20b86e47d
Ryan Yang <ryanyanyang@live.com>
2016-10-27 22:31:11 -0700

    Finally got node

--- README.md
+++
@@ -1,2 +0,0 @@
-# Outlts
-Outlts is an app that helps people who destress creatively share hteir creations.
--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
+// Tell express that the html files are located in a specific directory
+app.set('views', __dirname + '/static');
+// Render the html files with view engine
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+// Tell express where the images are
+app.use(express.static(__dirname + '/static'));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname +'/static/home.html');
-  res.sendFile(__dirname +'/static/editprofile.html');
+	res.render("home");
+	res.sendFile(__dirname +'/static/home.html');
+	res.sendFile(__dirname +'/static/editprofile.html');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+	console.log('Example app listening on port 3000!');
 });

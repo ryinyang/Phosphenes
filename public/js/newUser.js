f014eaf309fb1034d1d85b0db0214dabb3514caf
Ryan Yang <ryanyanyang@live.com>
2016-11-09 16:52:46 -0800

    Got account creation mostly done. Login and new user seem to load separate instances of the database so they can't work with each other yet.

--- public/js/newUser.js
+++ public/js/newUser.js
@@ -1,8 +1,6 @@
 var loginDatabase = jQuery.getJSON("js/login.json");
-var first = false;
 
 $(document).ready(function(){
-	$("#login-btn").click(checkLogin);
 	$("#create-btn").click(createAccount);
 	$("#back-btn").click(backButton);
 });
@@ -10,35 +8,6 @@ $(document).ready(function(){
 function backButton(){
 	window.location.href = "/";
 }
-
-function checkLogin(){
-
-	// Look at only the array of the json
-	var  data = loginDatabase.responseJSON;
-
-	// Loop through the database and check for the correct uname/pword
-	for (var i = data.length - 1; i >= 0; i--) {
-		if ($("#uname-input").val() == data[i].uname && 
-			$("#pword-input").val() == data[i].pword) {
-			console.log("correct");
-			window.location.href = "/home";
-			return;
-		}
-	};
-	alert("You've entered the incorrect uname or password.");
-	console.log(loginDatabase.responseJSON);
-}
-
-// var loginDatabase = jQuery.getJSON("js/login.json");
-
-// $(document).ready(function(){
-// 	$("#create-btn").click(createAccount);
-// 	$("#back-btn").click(backButton);
-// });
-
-// function backButton(){
-// 	window.location.href = "/";
-// }
 
 function createAccount(){
 	var data = loginDatabase.responseJSON;
@@ -63,12 +32,10 @@ function createAccount(){
 			return;
 		}
 		
-		// Push new account into "database"
 		loginDatabase.responseJSON.push({
 			uname: uname,
 			pword: pword1
 		});
 		console.log(loginDatabase.responseJSON);
-		window.location.href = "/home";
 	};
 }

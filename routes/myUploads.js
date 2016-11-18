b32ee5e6b76a84ca936a8af8eb81207f27fbc7ab
Ryan Yang <ryanyanyang@live.com>
2016-11-17 18:38:09 -0800

    Pushing to test on phone

--- routes/myUploads.js
+++ routes/myUploads.js
@@ -1,5 +1,9 @@
-var myUploads = require('../myUploads.json');
+// var myUploads = require('../myUploads.json');
+
+var fs = require('fs');
 
 exports.view = function(req, res) {
+	var myUploads = fs.readFileSync('myUploads.json');
+	myUploads = JSON.parse(myUploads);
 	res.render('myUploads', myUploads);
 };
